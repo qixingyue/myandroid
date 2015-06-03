c77ddf71de78a69457c68ca254a8aa36cb50cd60
yanqing4 <yanqing4@staff.sina.com.cn>
2015-06-03 11:02:03 +0800

    delete data init in RadarView

--- src/com/weibo/libs/RadarView.java
+++ src/com/weibo/libs/RadarView.java
@@ -2,9 +2,6 @@ package com.weibo.libs;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
-//import com.yq.async.RemoteModel;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -24,11 +21,11 @@ public class RadarView extends FrameLayout {
 	private Paint mPaintSector;
 	private Paint mPaintSector1;
 	private Paint mPaintText;
-	
+
 	public boolean isstart = false;
 	private Timer mTimer;
 	private int start = 0;
-	
+
 	private static final int WIDTH = 720;
 	private static final int HEIGHT = 1200;
 	private static final int FONT_SIZE = 35;
@@ -36,14 +33,7 @@ public class RadarView extends FrameLayout {
 	private float radioHeight = 1f;
 	private float radioWidth = 1f;
 	private int fontSize = 35;
-	
-	
-//	private RemoteModel showPriceModel = new RemoteModel();
 
-//	public void setRemoteModel(RemoteModel showPriceModel){
-//		this.showPriceModel = showPriceModel;
-//	}
-	
 	public RadarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setBackgroundColor(Color.TRANSPARENT);
@@ -53,7 +43,7 @@ public class RadarView extends FrameLayout {
 	public RadarView(Context context) {
 		this(context, null);
 	}
-	
+
 	private void initPaint() {
 		mPaintLine = new Paint();
 		mPaintLine.setStrokeWidth(1);
@@ -62,7 +52,6 @@ public class RadarView extends FrameLayout {
 		mPaintLine.setColor(Color.parseColor("#FFC1C1"));
 
 		mPaintSector = new Paint();
-		// mPaintSector.setColor(Color.parseColor("#FFE4E1"));
 		mPaintSector.setStyle(Style.STROKE);
 		mPaintSector.setStrokeWidth(radarSize / 10);
 		mPaintSector.setAlpha(100);
@@ -73,20 +62,29 @@ public class RadarView extends FrameLayout {
 		mPaintSector1.setStrokeWidth(3);
 		mPaintSector.setAlpha(100);
 		mPaintSector1.setAntiAlias(true);
-		
+
 		mPaintText = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DEV_KERN_TEXT_FLAG);
 		mPaintText.setTextSize(fontSize);
+
+		Shader mShader = new SweepGradient(viewSize / 2, viewSize / 2,
+				new int[] { Color.WHITE, Color.parseColor("#FFE4E1") },
+				new float[] { 0.5f, 1 });
+		mPaintSector.setShader(mShader);
+
+		mShader = new SweepGradient(viewSize / 2, viewSize / 2, new int[] {
+				Color.WHITE, Color.parseColor("#EE3B3B") }, null);
+		mPaintSector1.setShader(mShader);
 	}
 
 	public void setradarSize(int width, int height) {
 		this.viewSize = width - width / 8;
 		this.radarSize = width - width / 8;
-		
-		this.radioWidth = (float)width / WIDTH;  
-		this.radioHeight = (float)height / HEIGHT; 
+
+		this.radioWidth = (float) width / WIDTH;
+		this.radioHeight = (float) height / HEIGHT;
 		this.radio = Math.min(this.radioWidth, this.radioHeight);
 		this.fontSize = Math.round(FONT_SIZE * radio);
-		
+
 		setMeasuredDimension(viewSize, viewSize);
 	}
 
@@ -111,63 +109,71 @@ public class RadarView extends FrameLayout {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		if (mPaintLine == null){
+		if (mPaintLine == null) {
 			initPaint();
 		}
-		
+
 		float baseLeft = viewSize / 2 - 80 * radioWidth;
-		float baseTop =  viewSize / 2 - 80 * radioHeight;
-		
-		/*draw text */
+		float baseTop = viewSize / 2 - 80 * radioHeight;
+
+		/* draw text */
 		mPaintText.setColor(Color.GREEN);
-//		canvas.drawText("买入: " + this.showPriceModel.getInnerPrice(), baseLeft , baseTop, mPaintText);
-//	
-//		mPaintText.setColor(Color.RED);
-//		canvas.drawText("卖出: " + this.showPriceModel.getOutPrice(), baseLeft, baseTop + 45 * radioHeight, mPaintText);
-//		
-//		mPaintText.setColor(Color.BLACK);
-//		canvas.drawText("最高: " + this.showPriceModel.getTodayHigh(), baseLeft, baseTop + 95 * radioHeight, mPaintText);
-//		canvas.drawText("最低: " + this.showPriceModel.getTodayLow(), baseLeft, baseTop + 135 * radioHeight, mPaintText);
-//		
-//		String dirStr = this.showPriceModel.getDirection();
-//		
-		canvas.drawText("买入: 000" , baseLeft , baseTop, mPaintText);
-		canvas.drawText("卖出: 000", baseLeft, baseTop + 45 * radioHeight, mPaintText);
-		
+		// canvas.drawText("买入: " + this.showPriceModel.getInnerPrice(),
+		// baseLeft , baseTop, mPaintText);
+		//
+		// mPaintText.setColor(Color.RED);
+		// canvas.drawText("卖出: " + this.showPriceModel.getOutPrice(), baseLeft,
+		// baseTop + 45 * radioHeight, mPaintText);
+		//
+		// mPaintText.setColor(Color.BLACK);
+		// canvas.drawText("最高: " + this.showPriceModel.getTodayHigh(),
+		// baseLeft, baseTop + 95 * radioHeight, mPaintText);
+		// canvas.drawText("最低: " + this.showPriceModel.getTodayLow(), baseLeft,
+		// baseTop + 135 * radioHeight, mPaintText);
+		//
+		// String dirStr = this.showPriceModel.getDirection();
+		//
+		canvas.drawText("买入: 000", baseLeft, baseTop, mPaintText);
+		canvas.drawText("卖出: 000", baseLeft, baseTop + 45 * radioHeight,
+				mPaintText);
+
 		mPaintText.setColor(Color.BLACK);
-		canvas.drawText("最高: " + "000", baseLeft, baseTop + 95 * radioHeight, mPaintText);
-		canvas.drawText("最低: " + "000", baseLeft, baseTop + 135 * radioHeight, mPaintText);
-		
+		canvas.drawText("最高: " + "000", baseLeft, baseTop + 95 * radioHeight,
+				mPaintText);
+		canvas.drawText("最低: " + "000", baseLeft, baseTop + 135 * radioHeight,
+				mPaintText);
+
 		String dirStr = "U";
-		
-		
-		String  direction = (dirStr.equals("D")) ? "下跌" : "上涨";
-		if(direction.equals("上涨")){
+
+		String direction = (dirStr.equals("D")) ? "下跌" : "上涨";
+		if (direction.equals("上涨")) {
 			mPaintText.setColor(Color.RED);
-		}else{
+		} else {
 			mPaintText.setColor(Color.GREEN);
 		}
-		canvas.drawText("趋势: " + direction, baseLeft, baseTop + 175 * radioHeight, mPaintText);
-		
+		canvas.drawText("趋势: " + direction, baseLeft, baseTop + 175
+				* radioHeight, mPaintText);
+
 		canvas.drawCircle(viewSize / 2, viewSize / 2, radarSize / 4, mPaintLine);
 		// canvas.drawCircle(radarSize / 2, radarSize / 2, 110, mPaintLine);
-		canvas.drawCircle(viewSize / 2, viewSize / 2, radarSize * 9 / 20,mPaintLine);
-		
-		canvas.drawLine(viewSize / 2, viewSize / 2 - (radarSize * 9 / 20), viewSize / 2, viewSize / 2 + (radarSize * 9 / 20), mPaintLine);
-		canvas.drawLine(viewSize / 2 - (radarSize * 9 / 20), viewSize / 2, viewSize / 2 + (radarSize * 9 / 20), viewSize / 2, mPaintLine);
-		
-//		Shader mShader = new SweepGradient(viewSize / 2, viewSize / 2, new int[] { Color.WHITE, Color.parseColor("#FFE4E1") },new float[] { 0.5f, 1 }); mPaintSector.setShader(mShader);
-//		mShader = new SweepGradient(viewSize / 2, viewSize / 2, new int[] {Color.WHITE, Color.parseColor("#EE3B3B") }, null);
-//		mPaintSector1.setShader(mShader);
-		
+		canvas.drawCircle(viewSize / 2, viewSize / 2, radarSize * 9 / 20,
+				mPaintLine);
+
+		canvas.drawLine(viewSize / 2, viewSize / 2 - (radarSize * 9 / 20),
+				viewSize / 2, viewSize / 2 + (radarSize * 9 / 20), mPaintLine);
+		canvas.drawLine(viewSize / 2 - (radarSize * 9 / 20), viewSize / 2,
+				viewSize / 2 + (radarSize * 9 / 20), viewSize / 2, mPaintLine);
+
 		if (matrix == null) {
 			matrix = new Matrix();
 		}
-		
+
 		canvas.concat(matrix);
-		canvas.drawCircle(viewSize / 2, viewSize / 2, radarSize * 8 / 20,mPaintSector);
-		canvas.drawCircle(viewSize / 2, viewSize / 2, (radarSize * 7 / 20) + 1, mPaintSector1);
-		
+		canvas.drawCircle(viewSize / 2, viewSize / 2, radarSize * 8 / 20,
+				mPaintSector);
+		canvas.drawCircle(viewSize / 2, viewSize / 2, (radarSize * 7 / 20) + 1,
+				mPaintSector1);
+
 		super.onDraw(canvas);
 	}
 

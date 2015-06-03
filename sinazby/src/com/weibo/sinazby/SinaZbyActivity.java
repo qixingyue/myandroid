42fb5e5e5b621128a136b5256668ee919664719d
xingyue <qixingyue@126.com>
2015-06-03 18:35:09 +0800

    添加卫星菜单功能

--- sinazby/src/com/weibo/sinazby/SinaZbyActivity.java
+++ sinazby/src/com/weibo/sinazby/SinaZbyActivity.java
@@ -1,18 +1,27 @@
 package com.weibo.sinazby;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.weibo.fragment.ZbyFragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.ext.*;
+import android.view.ext.SatelliteMenu.SateliteClickedListener;
+
 
 public class SinaZbyActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);   
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sina_zby);
 
@@ -22,11 +31,27 @@ public class SinaZbyActivity extends FragmentActivity {
 		if (null == fragment) {
 			fragment = new ZbyFragment();
 			// fragment = createFragment();
-			manager
-			.beginTransaction()
-			.add(R.id.fragmentContainer, fragment)
-		     .commit();
+			manager.beginTransaction().add(R.id.fragmentContainer, fragment)
+					.commit();
 		}
+
+		initMenu();
+
+	}
+
+	private void initMenu() {
+		SatelliteMenu menu = (SatelliteMenu) findViewById(R.id.menu);
+		List<SatelliteMenuItem> items = new ArrayList<SatelliteMenuItem>();
+		items.add(new SatelliteMenuItem(4, R.drawable.ic_1));
+		items.add(new SatelliteMenuItem(4, R.drawable.ic_3));
+		items.add(new SatelliteMenuItem(1, R.drawable.ic_2));
+		menu.addItems(items);
+		menu.setOnItemClickedListener(new SateliteClickedListener() {
+
+			public void eventOccured(int id) {
+				Log.i("sat", "Clicked on " + id);
+			}
+		});
 	}
 
 	@Override
